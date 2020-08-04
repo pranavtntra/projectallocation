e8da2b7e48f5597fe3d2c9e35f62834332573479
Pranav Modh <pranav.modh@botreetechnologies.com>
2020-08-04 22:33:50 +0530

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn majorproject.wsgi --log-file -
--- majorproject/settings.py
+++ majorproject/settings.py
@@ -163,3 +163,4 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
